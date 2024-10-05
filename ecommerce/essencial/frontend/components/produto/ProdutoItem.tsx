b4585561857599d27e0e4afa447f5d7a3618a305
ROBERTO CASTRO <37387009+betokast@users.noreply.github.com>
2024-10-05 00:30:27 +0000

    Implementação do primeiro componente

--- ecommerce/essencial/frontend/components/produto/ProdutoItem.tsx
+++ ecommerce/essencial/frontend/components/produto/ProdutoItem.tsx
@@ -1,4 +1,7 @@
+'use client'
 import { Produto } from "@/core"
+import { IconShoppingCartPlus } from "@tabler/icons-react"
+import Image from "next/image"
 import Link from "next/link"
 
 export interface ProdutoItemProps {
@@ -10,7 +13,42 @@ export default function ProdutoItem(props: ProdutoItemProps) {
     return <Link href={`/produto/${produto.id}`}
                 className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
             >
-                { props.produto.nome }
+                <div className="h-48 w-full relative">
+                    <Image className="object-contain"
+                        src={produto.imagem}
+                        fill
+                        alt='Imagem do produto'
+                     />
+                </div>
+                <div className="flex-1 flex flex-col p-5 gap-3 border-t border-white/10">
+                    <span className="text-lg font-semibold">
+                        { props.produto.nome }
+                    </span>
+                    <span className="text-sm border-b border-dashed self-start border-white/50">
+                        { produto.especificacoes.destaque }
+                    </span>
+                    <div className="flex-1"></div>
+                    <div className="flex flex-col">
+                        <span className="text-sm text-gray-400 line-through">
+                            de {props.produto.precoBase}
+                        </span>
+                        <span className="text-xl font-semibold text-emerald-400">
+                            por {props.produto.precoPromocional}
+                        </span>
+                    </div>
+                    <button className="
+                        flex justify-center h-8 gap-2 items-center
+                        bg-violet-700 rounded-full hover:border-2 border-emerald-500 "
+
+                        onClick={(e:any) => {
+                            e.preventDefault()       
+                            console.log("Adicionado ao carrinho")
+                        }}
+                    >                        
+                        <IconShoppingCartPlus size={20} />
+                        <span>Adicionar</span>
+                    </button>
+                </div>
             </Link>
 }
 
